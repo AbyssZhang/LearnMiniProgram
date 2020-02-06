e282c92993cd509bf11e7dad1e750a3216b90d3f
ZhangHan <1755331@qq.com>
2020-02-06 16:33:44 +0800

    内置组件

---
+++ pages/image/image.js
@@ -0,0 +1,28 @@
+// pages/image/image.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+  handleChooseAlbum(){
+    // console.log('-----------')
+    wx.chooseImage({
+      success: (res)  => {
+        console.log(res)
+        // 1.取出路径
+        const path = res.tempFilePaths[0];
+        // 2.设置imagePath
+        this.setData({
+          imagePath: path
+        })
+      },
+    })
+  
+  },
+  handleImageLoad() {
+    console.log('图片加载完成')
+  }
+})
