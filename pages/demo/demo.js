e3d8d0095c2ad76f522bd344d18dc47bcb0b6690
ZhangHan <1755331@qq.com>
2020-02-08 22:50:59 +0800

    047-048

--- pages/demo/demo.js
+++ pages/demo/demo.js
@@ -61,6 +61,64 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
+    return {
+      title:'你好啊',
+      path:'/pages/index/index'
+    }
+  },
 
+  handleShowToast(){
+    wx.showToast({
+      title: '加载中ing',
+      duration:3000,
+      icon:'loading',
+      mask:true,
+      success:function(){
+        console.log('展示弹窗成功')
+      },
+      fail:function(){
+        console.log('展示弹窗失败')
+      },
+      complete:function(){
+        console.log('完成函数的调用')
+      }
+    })
+  },
+  handleShowModal(){
+    wx.showModal({
+      title:'title',
+      content:'content',
+      // showCancel:false,
+      cancelText:'退出',
+      cancelColor:'red',
+      success:function(res){
+        console.log(res)
+        if(res.cancel){
+          console.log('用户点击了取消')
+        }
+        if(res.confirm){
+          console.log('用户点击了确定')
+        }
+      }
+    })
+  },
+  handleShowLoading(){
+    wx.showLoading({
+      title: '加载ing',
+      mask:true
+    })
+    
+    setTimeout(()=>{
+      wx.hideLoading()
+    },1000)
+  },
+  handleShowAction(){
+    wx.showActionSheet({
+      itemList: ['相册','拍照'],
+      itemColor:'red',
+      success:function(res){
+        console.log(res)
+      }
+    })
   }
 })
